4236fbc8d36dd8d4a11bcd80b29aef1facadfcd0
David von Knobelsdorff <david.vonknobelsdorff@dm.de>
2023-01-02 15:25:51 +0100

    remove sync command as this is done via GitHub Actions

--- Sources/main/MainCommand.swift
+++ Sources/main/MainCommand.swift
@@ -21,27 +21,6 @@ struct Build: ParsableCommand {
     }
 }
 
-struct Sync: AsyncParsableCommand {
-    mutating func run() async throws {
-        let site = try Site(baseDirectory: Main.path)
-
-//        let configuration = try JSONDecoder().decode(S3Uploader.Configuration.self, from: Data(contentsOf: Main.path.appendingPathComponent("config.json")))
-
-//        print("Building & syncing the site to \(configuration.bucket).")
-
-//        let uploader = S3Uploader(configuration: configuration)
-        #warning("upload to github pages")
-        let resources = try site.generate()
-
-        await withTaskGroup(of: Void.self) { group in
-            for resource in resources {
-                let _ = group.addTaskUnlessCancelled {
-//                    try? await uploader.uploadIfNeeded(resource: resource)
-                }
-            }
-        }
-    }
-}
 
 @main
 struct Main: AsyncParsableCommand {
@@ -50,8 +29,7 @@ struct Main: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "A static site generator for https://davidvonk.dev",
         subcommands: [
-            Build.self,
-            Sync.self
+            Build.self
         ],
         defaultSubcommand: Build.self)
 }
