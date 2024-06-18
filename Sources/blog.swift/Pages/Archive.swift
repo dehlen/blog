11a97cd651f9d2aa1e1b5c293410991cb0cddc11
David v.Knobelsdorff <dehlen@users.noreply.github.com>
2024-06-18 19:19:33 +0200

    Archive.swift aktualisieren

--- Sources/blog.swift/Pages/Archive.swift
+++ Sources/blog.swift/Pages/Archive.swift
@@ -9,17 +9,36 @@ struct Archive: Page {
     let pathComponents = [ "archive" ]
 
     func content() -> Node {
-        ul {
-            posts
-                .reversed()
-                .map { post in
-                    li {
-                        "[ \(format(post.date)) ] "
-                        a(href: post.path) {
-                            post.title
+        posts.sliced(by: [.year], for: \.date).map { (key: Date, value: [Post]) -> Node in
+            [
+                h2 { "\(key.formatted(.year()))" },
+                ul {
+                    value
+                        .reversed()
+                        .map { post in
+                            li {
+                                "[ \(format(post.date)) ] "
+                                a(href: post.path) {
+                                    post.title
+                                }
+                            }
                         }
-                    }
                 }
-        }
+            ]
+        }.flatMap { $0 }
     }
+}
+
+private extension Array {
+  func sliced(by dateComponents: Set<Calendar.Component>, for key: KeyPath<Element, Date>) -> [Date: [Element]] {
+    let initial: [Date: [Element]] = [:]
+    let groupedByDateComponents = reduce(into: initial) { acc, cur in
+      let components = Calendar.current.dateComponents(dateComponents, from: cur[keyPath: key])
+      let date = Calendar.current.date(from: components)!
+      let existing = acc[date] ?? []
+      acc[date] = existing + [cur]
+    }
+
+    return groupedByDateComponents
+  }
 }
